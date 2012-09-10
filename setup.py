012992c9b9f556acc3a5d62da87e3bc60d596ae8
Jeremy A <jeremy@obsoleter.com>
2012-09-10 10:56:53 -0400

    add license. add setuptools setup.py.

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('readme.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='sysutils',
+    version='0.0.1',
+    description='Simple helpers for systems managements',
+    long_description=readme,
+    author='Jeremy Axmacher',
+    author_email='jeremy@obsoleter.com',
+    url='https://github.com/obsoleter/sysutils',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
